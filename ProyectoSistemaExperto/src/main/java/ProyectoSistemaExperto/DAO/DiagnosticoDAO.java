7cd8aee1eaa268dc86ae15e21a37fd80a83eca43
Johan Andrés Villada <vvj1998@protonmail.com>
2025-12-10 17:56:46 -0500

    Daos iniciales conectados

--- ProyectoSistemaExperto/src/main/java/ProyectoSistemaExperto/DAO/DiagnosticoDAO.java
+++ ProyectoSistemaExperto/src/main/java/ProyectoSistemaExperto/DAO/DiagnosticoDAO.java
@@ -8,6 +8,24 @@ package ProyectoSistemaExperto.DAO;
  *
  * @author Johan Villada
  */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DiagnosticoDAO {
-    
+
+    public void guardarDiagnostico(int idPaciente, int idEnfermedad) {
+        String sql = "INSERT INTO diagnostico(id_paciente, id_enfermedad) VALUES (?, ?)";
+
+        try (Connection con = ConexionJava.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, idPaciente);
+            ps.setInt(2, idEnfermedad);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
