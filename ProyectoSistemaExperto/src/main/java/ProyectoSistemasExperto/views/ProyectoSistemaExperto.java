6465eab18246f8eb5f0ca22acf16cfd62e5e0a16
unknown <evers.uribeq@autonoma.edu.co>
2025-12-10 22:48:33 -0500

    pruebas interfaz de usuario

---
+++ ProyectoSistemaExperto/src/main/java/ProyectoSistemasExperto/views/ProyectoSistemaExperto.java
@@ -0,0 +1,16 @@
+
+package ProyectoSistemasExperto.views;
+import com.formdev.flatlaf.FlatLightLaf;
+/**
+ *
+ * @author EVER URIBE
+ */
+public class ProyectoSistemaExperto {
+    public static void main(String[] args) {
+        FlatLightLaf.setup(); // Activar apariencia moderna FlatLaf
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new VentanaBienvenida().setVisible(true);
+        });
+    }
+}
